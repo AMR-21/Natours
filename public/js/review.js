a8bda59f6d9121333d4d0bff622ed144bf3146fb
Amr Yasser <amryasser52001@gmail.com>
2023-05-07 15:46:09 +0300

    Receive By Amr

--- public/js/review.js
+++ public/js/review.js
@@ -2,25 +2,25 @@ import axios from 'axios';
 import { showAlert } from './alert';
 
 export const addReview = async (tour, user, review, rating) => {
-    try {
-        const res = await axios({
-            method: 'POST',
-            url: '/api/v1/reviews',
-            data: {
-                tour,
-                user,
-                review,
-                rating,
-            },
-        });
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: '/api/v1/reviews',
+      data: {
+        tour,
+        user,
+        review,
+        rating,
+      },
+    });
 
-        if (res.data.status === 'success') {
-            showAlert('success', 'Thank You For Your Review');
-            window.setTimeout(() => {
-                location.assign('/');
-            }, 1500);
-        }
-    } catch (err) {
-        showAlert('error', "You can't add more than one review for the same tour");
+    if (res.data.status === 'success') {
+      showAlert('success', 'Thank You For Your Review');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
     }
-}
+  } catch (err) {
+    showAlert('error', "You can't add more than one review for the same tour");
+  }
+};
